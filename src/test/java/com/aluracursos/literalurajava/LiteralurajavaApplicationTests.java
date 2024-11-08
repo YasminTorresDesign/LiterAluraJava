139f94fcafaba4163475eef372111cfbc6f145a2
YasminTorresDesign <yatorres.73@gmail.com>
2024-11-07 19:11:07 -0500

    Construyendo el servicio de solicitud a la api

---
+++ src/test/java/com/aluracursos/literalurajava/LiteralurajavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.literalurajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteralurajavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
